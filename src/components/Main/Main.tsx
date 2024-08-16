2ec69900d8f60808628dbba8ffa498836cb9607b
Ayan <ilovememesdontkillme@gmail.com>
2024-08-16 19:50:15 +0900

    main layout

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,11 +1,29 @@
 import './Main.style.scss'
-
+import DescriptionIcon from '@mui/icons-material/Description';
 export default function Main() {
     return(
         <div className="main">
             <div className='main-inner'>
-
-            <p>bjnjasfndkl</p>popo
+            
+            <div className='table'>
+                <div className='header'>
+                    <p>Уровень</p>
+                    <p className='second-col'>Название работ</p>
+                    <p>Основная з/п</p>
+                    <p>Обородувание</p>
+                    <p>Накладные расходы</p>
+                    <p>Сметная прибыль</p>
+                </div>
+                <div className='row'>
+                    <DescriptionIcon color='primary' className='icon'/>
+                    <input className='second-col' />
+                    <input />
+                    <input />
+                    <input />
+                    <input />
+                </div>
+            </div>
+            
             </div>
         </div>
     )
